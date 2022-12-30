0dde6be19b5af8d99767c5d061338f07861c10d9
vitarttuva <pro1001000r@ya.ru>
2022-12-30 13:40:52 +0700

    1

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -1,23 +1,16 @@
 import React, { useEffect, useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  Button,
-  Image,
-} from "react-native";
+import { StyleSheet, Text, View, TextInput, Button, Image } from "react-native";
 import axios from "axios";
 
 export default function MainScreen({ navigation, route }) {
-  console.log('route');
+  console.log("route");
 
   const [login, setLogin] = useState("");
   const [password, setPassword] = useState("");
- 
+
   useEffect(() => {}, []);
 
-const vAuth = (log,pass) => {
+  const vAuth = (log, pass) => {
     const config = {
       headers: {
         "Content-Type": "application/json",
@@ -26,19 +19,21 @@ const vAuth = (log,pass) => {
     };
 
     const apiUrl = "http://terminal17.ru/Ajax/Obmen/}";
-    const data = { mobileLogin: {login: log, password: pass,} };
+    const data = { mobileLogin: { login: log, password: pass } };
 
     axios
       .post(apiUrl, data, config)
       .then(function (response) {
         //setNomen(response.data);
         console.log(response.data);
+        if (response.data != null){
+            navigation.navigate("Main");
+        }
       })
       .catch(function (error) {
         //setNomen(error);
       });
   };
-
 
   return (
     <View style={styles.container}>
@@ -51,18 +46,18 @@ const vAuth = (log,pass) => {
       {/* здесь делаем вход */}
       <TextInput
         style={styles.text}
-        value = {login}
+        value={login}
         onChangeText={setLogin}
         placeholder="Введите логин"
       />
       <TextInput
         style={styles.text}
-        value = {password}
+        value={password}
         onChangeText={setPassword}
         placeholder="Введите пароль"
       />
       {/* <Button title="Вход" onPress={() => navigation.navigate("Find")} /> */}
-      <Button title="Вход" onPress={() => vAuth(login,password)} />
+      <Button style={styles.vButton} title="Вход" onPress={() => vAuth(login, password)} />
     </View>
   );
 }
@@ -72,17 +67,25 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "#fff",
   },
   text: {
     padding: 3,
     margin: 3,
     borderWidth: 1,
     width: 250,
-    borderRadius: 5, 
+    borderRadius: 5,
     fontSize: 16,
+    textAlign: "center",
   },
   vImage: {
+    flex: 1,
     width: "50%",
     height: "50%",
+  },
+  vButton: {
+    flex: 1,
+    borderRadius: 5,
+    borderWidth: 1,
   },
 });
